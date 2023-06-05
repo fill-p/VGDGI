542cd3c0cf671f51699be834ca90f2c5efbb8495
fill-p <83204197+fill-p@users.noreply.github.com>
2023-06-05 22:59:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vite';
-
+import mkcert from'vite-plugin-mkcert'
 export default defineConfig({
   base: '/',
-  server: { https: false },
-  plugins: [],
+  server: { https: true },
+  plugins: [
+    mkcert()
+  ],
 });
